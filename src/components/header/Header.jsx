742b5c0bed85eaf36a6b58221cf9acd3fe3c89d0
Jose Bayona <bayonaj368@gmail.com>
2022-09-30 10:07:49 -0500

    agregar tailwind y pasar header a tailwind

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,14 +4,30 @@ import Spline from '@splinetool/react-spline';
 export const Header = () => {
     return (
 
-        <Grid.Container justify="center" css={{ mt:"1rem" }}>
-            <Grid xs={11} sm={7} md={7} lg={7} xl={7} direction="column" justify="center" >
-                <Text h1 size={70} em>You, Your Cattle & The System</Text>
-                <Text h2 hideIn="xs">Automated cattle certification via NFT</Text>
-            </Grid>
-            <Grid xs={11} sm={4} md={4} lg={4} xl={4} css={{ aspectRatio: '1/1' }}>
-                {/* <Spline scene="https://prod.spline.design/3RcuLnlMwt6kZbmt/scene.splinecode" /> */}
-            </Grid>
-        </Grid.Container>
+        <header className="flex w-full justify-center items-center">
+            <div className="flex flex-col sm:flex-row w-11/12">
+
+                <div className="flex flex-col justify-center content-center gap-y-7 w-full sm:w-3/5">
+                    <p className="text-6xl sm:text-7xl italic font-bold">You, Your Cattle & The System</p>
+                    <p className="text-xl sm:text-3xl">Automated cattle certification via NFT</p>
+                </div>
+
+                <div className="w-full sm:w-2/5 h-96">
+                    <Spline scene="https://prod.spline.design/3RcuLnlMwt6kZbmt/scene.splinecode" />
+                </div>
+
+            </div>
+        </header>
+
+
+        // <Grid.Container justify="center" css={{ mt:"1rem" }}>
+        //     <Grid xs={11} sm={7} md={7} lg={7} xl={7} direction="column" justify="center" >
+        //         <Text h1 size={70} em>You, Your Cattle & The System</Text>
+        //         <Text h2 hideIn="xs">Automated cattle certification via NFT</Text>
+        //     </Grid>
+        //     <Grid xs={11} sm={4} md={4} lg={4} xl={4} css={{ aspectRatio: '1/1' }}>
+        //         {/* <Spline scene="https://prod.spline.design/3RcuLnlMwt6kZbmt/scene.splinecode" /> */}
+        //     </Grid>
+        // </Grid.Container>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import './main.css'
 import { NextUIProvider } from '@nextui-org/react'
 import { darkTheme } from './themes/darktheme'
 
+
 import { App } from './App'
 
-// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <NextUIProvider theme={ darkTheme }>
