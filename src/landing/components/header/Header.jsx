4a2cacd748c2c3b81481030a6c20bdcdf8dc0f28
jose bayona <bayonaj368@gmail.com>
2022-11-12 18:54:09 -0600

    agregar imagen a header

--- src/landing/components/header/Header.jsx
+++ src/landing/components/header/Header.jsx
@@ -5,15 +5,15 @@ export const Header = () => {
     return (
         <>
             <header className="flex w-full justify-center items-center">
-                <div className="flex flex-col sm:flex-row w-11/12">
+                <div className="flex flex-col sm:flex-row w-11/12 ">
 
                     <div className="flex flex-col justify-center content-center gap-y-7 w-full sm:w-3/5">
                         <p className="text-6xl sm:text-7xl italic font-bold">You, Your Cattle & The System</p>
                         <p className="text-xl sm:text-3xl">Automated cattle certification via NFT</p>
                     </div>
 
-                    <div className="w-full sm:w-2/5 h-96">
-                        {/* <Spline scene="https://prod.spline.design/3RcuLnlMwt6kZbmt/scene.splinecode" /> */}
+                    <div className="w-full mt-7 sm:w-2/5 sm:mt-0">
+                        <img alt="cloud nodes" src="assets/img/header/clouds-nodes.png" className="img-shape" />  
                     </div>
 
                 </div>
