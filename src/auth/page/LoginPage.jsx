d1a0f3910763e8fa3445819eb70a427125d4322d
jose bayona <bayonaj368@gmail.com>
2022-11-12 21:32:46 -0600

    ajustar layout de forms

--- src/auth/page/LoginPage.jsx
+++ src/auth/page/LoginPage.jsx
@@ -1,11 +1,9 @@
-import { FooterAuth } from "../components/footer/FooterAuth"
 import { LoginForm } from "../components/login/LoginForm"
 
 export const LoginPage = () => {
     return (
         <>
             <LoginForm />
-            <FooterAuth />
         </>
     )
 }
