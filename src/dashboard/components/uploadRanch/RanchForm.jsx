f77a1c937a7c039f6f68791f24147a87bec1a496
jose bayona <bayonaj368@gmail.com>
2022-11-20 23:13:13 -0600

    quitar card al from de upload ranch

--- src/dashboard/components/uploadRanch/RanchForm.jsx
+++ src/dashboard/components/uploadRanch/RanchForm.jsx
@@ -14,201 +14,194 @@ export const RanchForm = () => {
 
 
     return (
-        <div className="w-full flex justify-center items-center mb-5">
 
-            <div className="w-full lg:w-1/2">
+        <div className="px-3 md:px-10">
 
-                {
-                    ( status === 201 ) // 201 == created
-                        ? <SuccesMessage message='Ranch created'/>
-                        : ''
-                }
-
-                <Card css={{ border: 'none' }}>
-                    <Card.Body>
-
-                        <Formik
-                            initialValues={{
-                                ranchName: '',
-                                city: '',
-                                street: '',
-                                phoneNumber: '',
-                                postalCode: '',
-                                country: '',
-                                cowHeads: '',
-                            }}
-
-                            validationSchema={ validationSchemeUploadRanch }
-
-                            onSubmit={ ( values ) => {
-                                console.log( 'formulario enviado' )
-                                dispatch( uploadRanch( values ) ) // make a post petition through thunks to create a ranch
-                                // make that the table rerender with the new data changing the state of the table when we click the button
-                                // dispatch( setRerenderState() ) 
-                                dispatch( addRanch({ values }) )
-                            } }
-                        >
-                            
-                            {/* form props from Formik */}
-                            { ({ values, errors, touched, handleSubmit, handleChange, handleBlur }) => (
-
-                                <form className="w-full flex flex-col" onSubmit={ handleSubmit }>
-
-                                    <Spacer y={1.5}/>
-
-                                    <Input 
-                                        labelPlaceholder="Ranch name"
-                                        bordered
-                                        fullWidth
-                                        name="ranchName"
-                                        color={ !errors.ranchName ? 'primary' : 'error' }
-                                        value={ values.ranchName }
-                                        onChange={ handleChange }
-                                        onBlur={ handleBlur }
-                                        helperColor={ !errors.ranchName ? 'primary' : 'error' }
-                                        helperText={ touched.ranchName && errors.ranchName }
-                                        // if the input was touched and we have en error, show the error
-                                    />
-
-                                    <Spacer y={1.5}/>
-
-                                    <Input 
-                                        labelPlaceholder="Street"
-                                        bordered
-                                        fullWidth
-                                        name="street"
-                                        color={ !errors.street ? 'primary' : 'error' }
-                                        value={ values.street }
-                                        onChange={ handleChange }
-                                        onBlur={ handleBlur }
-                                        helperColor={ !errors.street ? 'primary' : 'error' }
-                                        helperText={ touched.street && errors.street }
-                                    />
-
-                                    <Spacer y={1.5}/>
-
-                                    <div className="flex">
-
-                                        <div className="w-1/2">
-                                            <Input 
-                                                labelPlaceholder="Country"
-                                                bordered
-                                                name="country"
-                                                fullWidth
-                                                color={ !errors.country ? 'primary' : 'error' }
-                                                value={ values.country }
-                                                onChange={ handleChange }
-                                                onBlur={ handleBlur }
-                                                helperColor={ !errors.country ? 'primary' : 'error' }
-                                                helperText={ touched.country && errors.country }
-                                            />
-                                        </div>
-
-                                        <Spacer  y={ 1.5 } />
-
-                                        <div className="w-1/2">
-                                            <Input 
-                                                labelPlaceholder="City"
-                                                bordered
-                                                name="city"
-                                                fullWidth
-                                                color={ !errors.city ? 'primary' : 'error' }
-                                                value={ values.city }
-                                                onChange={ handleChange }
-                                                onBlur={ handleBlur }
-                                                helperColor={ !errors.city ? 'primary' : 'error' }
-                                                helperText={ touched.city && errors.city }
-                                            />
-                                        </div>
-                                    </div>
-
-                                        <Spacer  y={ 1.5 } />
-
-                                    <div className="flex">
-
-                                        <div className="w-1/2">
-                                            <Input 
-                                                labelPlaceholder="Postal Code"
-                                                bordered
-                                                name="postalCode"
-                                                fullWidth
-                                                type="number"
-                                                min={ 0 }
-                                                color={ !errors.postalCode ? 'primary' : 'error' }
-                                                value={ values.postalCode }
-                                                onChange={ handleChange }
-                                                onBlur={ handleBlur }
-                                                helperColor={ !errors.postalCode ? 'primary' : 'error' }
-                                                helperText={ touched.postalCode && errors.postalCode }
-                                            />
-                                        </div>
-
-                                        <Spacer  y={ 1.5 } />
-
-                                        <div className="w-1/2">
-                                            <Input 
-                                                labelPlaceholder="Phone Number"
-                                                bordered
-                                                name="phoneNumber"
-                                                type="number"
-                                                fullWidth
-                                                color={ !errors.phoneNumber ? 'primary' : 'error' }
-                                                value={ values.phoneNumber }
-                                                onChange={ handleChange }
-                                                onBlur={ handleBlur }
-                                                helperColor={ !errors.phoneNumber ? 'primary' : 'error' }
-                                                helperText={ touched.phoneNumber && errors.phoneNumber }
-                                            />
-                                        </div>
-                                    </div>
-
-                                    <Spacer y={1.5}/>
-
-                                    <Input 
-                                        labelPlaceholder="Cow Heads"
-                                        bordered
-                                        name="cowHeads"
-                                        type="number"
-                                        fullWidth
-                                        color={ !errors.cowHeads ? 'primary' : 'error' }
-                                        value={ values.cowHeads }
-                                        onChange={ handleChange }
-                                        onBlur={ handleBlur }
-                                        helperColor={ !errors.cowHeads ? 'primary' : 'error' }
-                                        helperText={ touched.cowHeads && errors.cowHeads }
-                                    />
-
-                                    <Spacer y={1.5}/>
-
-                                    {
-                                        ( !isLoading )
-                                            ? <Input 
-                                                type="submit" 
-                                                aria-label="send-form"
-                                                bordered
-                                                fullWidth
-                                                css={{ bg: '$primary' }}
-                                                value='Upload Ranch'
-                                                animated='false' 
-                                            />
-                                            : <Loading type="points"/>
-                                    }
+            {
+                ( status === 201 ) // 201 == created
+                    ? <SuccesMessage message='Ranch created'/>
+                    : ''
+            }
 
 
-                                    <Spacer y={1.5}/>
+            <Formik
+                initialValues={{
+                    ranchName: '',
+                    city: '',
+                    street: '',
+                    phoneNumber: '',
+                    postalCode: '',
+                    country: '',
+                    cowHeads: '',
+                }}
 
-                                </form> 
+                validationSchema={ validationSchemeUploadRanch }
 
-                            ) }
+                onSubmit={ ( values ) => {
+                    console.log( 'formulario enviado' )
+                    dispatch( uploadRanch( values ) ) // make a post petition through thunks to create a ranch
+                    // make that the table rerender with the new data changing the state of the table when we click the button
+                    // dispatch( setRerenderState() ) 
+                    dispatch( addRanch({ values }) )
+                } }
+            >
+                
+                {/* form props from Formik */}
+                { ({ values, errors, touched, handleSubmit, handleChange, handleBlur }) => (
+
+                    <form className="w-full flex flex-col" onSubmit={ handleSubmit }>
+
+                        <Spacer y={1.5}/>
+
+                        <Input 
+                            labelPlaceholder="Ranch name"
+                            bordered
+                            fullWidth
+                            name="ranchName"
+                            color={ !errors.ranchName ? 'primary' : 'error' }
+                            value={ values.ranchName }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            helperColor={ !errors.ranchName ? 'primary' : 'error' }
+                            helperText={ touched.ranchName && errors.ranchName }
+                            // if the input was touched and we have en error, show the error
+                        />
+
+                        <Spacer y={1.5}/>
+
+                        <Input 
+                            labelPlaceholder="Street"
+                            bordered
+                            fullWidth
+                            name="street"
+                            color={ !errors.street ? 'primary' : 'error' }
+                            value={ values.street }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            helperColor={ !errors.street ? 'primary' : 'error' }
+                            helperText={ touched.street && errors.street }
+                        />
+
+                        <Spacer y={1.5}/>
+
+                        <div className="flex">
+
+                            <div className="w-1/2">
+                                <Input 
+                                    labelPlaceholder="Country"
+                                    bordered
+                                    name="country"
+                                    fullWidth
+                                    color={ !errors.country ? 'primary' : 'error' }
+                                    value={ values.country }
+                                    onChange={ handleChange }
+                                    onBlur={ handleBlur }
+                                    helperColor={ !errors.country ? 'primary' : 'error' }
+                                    helperText={ touched.country && errors.country }
+                                />
+                            </div>
+
+                            <Spacer  y={ 1.5 } />
+
+                            <div className="w-1/2">
+                                <Input 
+                                    labelPlaceholder="City"
+                                    bordered
+                                    name="city"
+                                    fullWidth
+                                    color={ !errors.city ? 'primary' : 'error' }
+                                    value={ values.city }
+                                    onChange={ handleChange }
+                                    onBlur={ handleBlur }
+                                    helperColor={ !errors.city ? 'primary' : 'error' }
+                                    helperText={ touched.city && errors.city }
+                                />
+                            </div>
+                        </div>
+
+                            <Spacer  y={ 1.5 } />
+
+                        <div className="flex">
+
+                            <div className="w-1/2">
+                                <Input 
+                                    labelPlaceholder="Postal Code"
+                                    bordered
+                                    name="postalCode"
+                                    fullWidth
+                                    type="number"
+                                    min={ 0 }
+                                    color={ !errors.postalCode ? 'primary' : 'error' }
+                                    value={ values.postalCode }
+                                    onChange={ handleChange }
+                                    onBlur={ handleBlur }
+                                    helperColor={ !errors.postalCode ? 'primary' : 'error' }
+                                    helperText={ touched.postalCode && errors.postalCode }
+                                />
+                            </div>
+
+                            <Spacer  y={ 1.5 } />
+
+                            <div className="w-1/2">
+                                <Input 
+                                    labelPlaceholder="Phone Number"
+                                    bordered
+                                    name="phoneNumber"
+                                    type="number"
+                                    fullWidth
+                                    color={ !errors.phoneNumber ? 'primary' : 'error' }
+                                    value={ values.phoneNumber }
+                                    onChange={ handleChange }
+                                    onBlur={ handleBlur }
+                                    helperColor={ !errors.phoneNumber ? 'primary' : 'error' }
+                                    helperText={ touched.phoneNumber && errors.phoneNumber }
+                                />
+                            </div>
+                        </div>
+
+                        <Spacer y={1.5}/>
+
+                        <Input 
+                            labelPlaceholder="Cow Heads"
+                            bordered
+                            name="cowHeads"
+                            type="number"
+                            fullWidth
+                            color={ !errors.cowHeads ? 'primary' : 'error' }
+                            value={ values.cowHeads }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                            helperColor={ !errors.cowHeads ? 'primary' : 'error' }
+                            helperText={ touched.cowHeads && errors.cowHeads }
+                        />
+
+                        <Spacer y={1.5}/>
+
+                        {
+                            ( !isLoading )
+                                ? <Input 
+                                    type="submit" 
+                                    aria-label="send-form"
+                                    bordered
+                                    fullWidth
+                                    css={{ bg: '$primary' }}
+                                    value='Upload Ranch'
+                                    animated='false' 
+                                />
+                                : <Loading type="points"/>
+                        }
 
 
-                        </Formik>
+                        <Spacer y={1.5}/>
 
-                    </Card.Body>
-                </Card>
+                    </form> 
 
-            </div>
+                ) }
+
+
+            </Formik>
 
         </div>
+
     )
 }
