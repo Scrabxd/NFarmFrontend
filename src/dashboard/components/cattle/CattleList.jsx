48bf94846838058e752c8e8dd302752d84c3210a
jose bayona <bayonaj368@gmail.com>
2022-11-22 19:34:11 -0600

    agregar spinner a your cattle page

--- src/dashboard/components/cattle/CattleList.jsx
+++ src/dashboard/components/cattle/CattleList.jsx
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux"
+import { LoadingSpinner } from "../loading/LoadingSpinner"
 import { NoElementsMessage } from "../messages/NoElementsMessage"
 import { CattleItem } from "./CattleItem"
 import { DropdownRanch } from "./DropdownRanch"
 
 export const CattleList = ({ ranches }) => {
 
-    const { cows } = useSelector( state => state.cows ) // reference to store -> reducer -> cows
+    const { cows, isLoading } = useSelector( state => state.cows ) // reference to store -> reducer -> cows
 
     return (
 
@@ -16,26 +17,24 @@ export const CattleList = ({ ranches }) => {
                     <DropdownRanch ranches={ ranches } /> 
                 </div>
 
+                {
+                    ( isLoading ) && <LoadingSpinner />
+                }
+
                 <div className="w-full flex flex-raw flex-wrap justify-center gap-4 items-center">
                     
                     {
                         ( cows.length != 0 )
                             ?
+                                ( !isLoading ) && <LoadingSpinner /> &&
                                 cows.map( ( cow, index ) => (
                                     <CattleItem key={ index } breed={ cow.breed } name={ cow.name } weight={ cow.weight } />
                                 ) )
                                 
-                            : <NoElementsMessage title="You don't have any cow on this ranch"/>
+                            : ( !isLoading ) && <LoadingSpinner /> && <NoElementsMessage title="You don't have any cow on this ranch"/>
                     }
 
                 </div>
-                {/* <div className="w-full flex flex-raw flex-wrap justify-center gap-4 items-center">
-                    {
-                        cows.map( ( cow, index ) => (
-                            <CattleItem key={ index } breed={ cow.breed } name={ cow.name } weight={ cow.weight } />
-                        ) )
-                    }
-                </div> */}
 
             </>
 
